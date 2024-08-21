aaf49ef3e4600120bdc7f91b282592d78991cf45
shifeng <xsfpku@gmail.com>
2024-08-21 17:26:32 +0800

    script/launch.sh: fix typo

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -51,7 +51,7 @@ mpiexec -n 8 python image_sample.py --batch_size 256 --training_mode consistency
 mpiexec -n 8 python image_sample.py --batch_size 32 --generator determ-indiv --training_mode consistency_distillation --sampler onestep --model_path /root/consistency/ct_bedroom256.pt --attention_resolutions 32,16,8 --class_cond False --use_scale_shift_norm False --dropout 0.0 --image_size 256 --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --num_samples 100 --resblock_updown True --use_fp16 True --weight_schedule uniform
 
 ######################################################################################
-# Tenary search for multi-step sampling on class-conditional ImageNet-64, and LSUN 256
+# Ternary search for multi-step sampling on class-conditional ImageNet-64, and LSUN 256
 ######################################################################################
 
 ## CD on ImageNet-64
